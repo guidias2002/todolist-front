dfef0adbb8cef4bd8f1d3509a3bfade0f615b972
guidias2002 <guidias200220@gmail.com>
2025-03-17 20:58:03 -0300

    stules: update css

--- src/components/EmptyList.tsx
+++ src/components/EmptyList.tsx
@@ -1,10 +1,7 @@
-import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
-
 const EmptyList = () => {
   return (
-    <div className="flex justify-center items-center p-4 bg-blue-100 border border-blue-500 rounded-md">
-      <AddCircleOutlineIcon className="text-blue-700" />
-      <span className="ml-4 text-blue-700">Nenhuma tarefa registrada.</span>
+    <div>
+      <span className="ml-4 text-black">Nenhuma tarefa registrada.</span>
     </div>
   );
 };
